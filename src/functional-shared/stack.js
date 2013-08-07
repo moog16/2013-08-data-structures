8a9d76211925e57289ba33adcc170261a4f3dff5
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:36:33 -0700

    stack.js functional-shared is for the most part working

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,32 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  var stack = {};
+
+  _.extend(stack, makeStack.stackMethods);
+
+  return stack;
+
+});
+
+makeStack.stackMethods = {};
+
+makeStack.stackMethods.push = function(value){
+  size++;
+  storage[size] = value;
+};
+
+makeStack.stackMethods.pop = function(){
+  var result = storage[size];
+  delete storage[size];
+  size && size--;
+  return result;
+};
+
+makeStack.stackMethods.size = function(){
+  return size;
 };
