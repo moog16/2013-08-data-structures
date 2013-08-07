8c1f1cd874561928060ade1ebc8db50470492241
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:07:08 -0700

    created dequeue, and modified enqueue and size to use begin and end variables

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -2,21 +2,27 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size = 0; // Hint: set an initial value here
+  var begin = 0; //next person's number in line
+
+  var end = 0; //next in line for roller coaster
 
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
-    storage[size] = value;
-    size++;
+    storage[begin] = value;
+    begin++;
   };
 
   queue.dequeue = function(){
+    var result = storage[end];
+    delete storage[end];
+    end++;
+    return result;
   };
 
   queue.size = function(){
-    return size;
+    return begin - end >= 0 ? begin-end : 0;
   };
 
   return queue;
